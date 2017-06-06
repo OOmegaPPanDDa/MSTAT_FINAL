77621cbac9ae5c7f315eb8bc7722b0a8b6f3c209
OOmegaPPanDDa <b02705027@ntu.edu.tw>
2017-06-06 17:33:13 +0800

    rank plot

--- test_rf.R
+++ test_rf.R
@@ -1,160 +1,78 @@
 library(readr)
 library(dplyr)
 library(randomForest)
+library(ggplot2)
 
 rank_stage = 500
+year_considered = c(2012,2013,2014,2015,2016)
 
-############################################################
-# load data
-pre_year = 2011
-year = 2012
-
-pre_year_path = paste0('./merge_data/merge_data_', pre_year, '.csv')
-year_path = paste0('./merge_data/merge_data_', year, '.csv')
-
-pre_year_data = read_csv(pre_year_path)
-year_data = read_csv(year_path)
+year_data_list = list()
+year_data_list_index = 1
 
 
-year_data <- year_data %>%
-  select(Player_Full_Name,Team,Salary)
-names(year_data)[ncol(year_data)] = 'Salary_to_predict' 
+for (the_year in year_considered){
+  
+  # load data
+  pre_year = the_year - 1
+  year = the_year
+  
+  pre_year_path = paste0('./merge_data/merge_data_', pre_year, '.csv')
+  year_path = paste0('./merge_data/merge_data_', year, '.csv')
+  
+  pre_year_data = read_csv(pre_year_path)
+  year_data = read_csv(year_path)
+  
+  
+  year_data <- year_data %>%
+    select(Player_Full_Name,Team,Salary)
+  names(year_data)[ncol(year_data)] = 'Salary_to_predict' 
+  
+  
+  # merge year and pre_year data
+  # merge by Player_Full_Name
+  the_data <- merge(x = pre_year_data, y = year_data, by = c('Player_Full_Name')) %>%
+    arrange(desc(Salary_to_predict))
+  
+  
+  the_data <- the_data[,-which(names(the_data) %in% c('Team.y'))]
+  
+  
+  # remove duplicated
+  name_duplicated_data = the_data$Player_Full_Name[duplicated(the_data$Player_Full_Name)]
+  # print(name_duplicated_data)
+  the_data <- the_data %>%
+    filter(!(Player_Full_Name %in% name_duplicated_data))
+  
+  
+  year_record = rep(the_year, nrow(the_data))
+  the_data = cbind(year_record, the_data)
+  
+  the_data <- the_data %>% 
+    mutate(Salary_rank = floor((rank(the_data$Salary, ties.method = 'last')/nrow(the_data))*rank_stage- 0.00001) + 1) %>% 
+    mutate(Salary_to_predict_rank = floor((rank(the_data$Salary_to_predict, ties.method = 'last')/nrow(the_data))*rank_stage- 0.00001) + 1)
+  
+  
+  
+  # scale data
+  the_data[8:23] = scale(the_data[8:23])
+  
+  year_data_list[[year_data_list_index]] = the_data
+  year_data_list_index = year_data_list_index + 1
+  
+  
+}
 
 
-# merge year and pre_year data
-data2012 <- merge(x = pre_year_data, y = year_data, by = c('Player_Full_Name', 'Team')) %>%
-  arrange(desc(Salary_to_predict))
-
-year_record = rep(2012, nrow(data2012))
-data2012 = cbind(year_record, data2012)
-
-data2012 <- data2012 %>% 
-  mutate(Salary_rank = floor((rank(data2012$Salary, ties.method = 'last')/nrow(data2012))*rank_stage- 0.00001) + 1) %>% 
-  mutate(Salary_to_predict_rank = floor((rank(data2012$Salary_to_predict, ties.method = 'last')/nrow(data2012))*rank_stage- 0.00001) + 1)
-
-
-############################################################
-# load data
-pre_year = 2012
-year = 2013
-
-pre_year_path = paste0('./merge_data/merge_data_', pre_year, '.csv')
-year_path = paste0('./merge_data/merge_data_', year, '.csv')
-
-pre_year_data = read_csv(pre_year_path)
-year_data = read_csv(year_path)
-
-
-year_data <- year_data %>%
-  select(Player_Full_Name,Team,Salary)
-names(year_data)[ncol(year_data)] = 'Salary_to_predict' 
-
-
-# merge year and pre_year data
-data2013 <- merge(x = pre_year_data, y = year_data, by = c('Player_Full_Name', 'Team')) %>%
-  arrange(desc(Salary_to_predict))
-
-year_record = rep(2013, nrow(data2013))
-data2013 = cbind(year_record, data2013)
-
-data2013 <- data2013 %>% 
-  mutate(Salary_rank = floor((rank(data2013$Salary, ties.method = 'last')/nrow(data2013))*rank_stage- 0.00001) + 1) %>% 
-  mutate(Salary_to_predict_rank = floor((rank(data2013$Salary_to_predict, ties.method = 'last')/nrow(data2013))*rank_stage- 0.00001) + 1)
-
-############################################################
-# load data
-pre_year = 2013
-year = 2014
-
-pre_year_path = paste0('./merge_data/merge_data_', pre_year, '.csv')
-year_path = paste0('./merge_data/merge_data_', year, '.csv')
-
-pre_year_data = read_csv(pre_year_path)
-year_data = read_csv(year_path)
-
-
-year_data <- year_data %>%
-  select(Player_Full_Name,Team,Salary)
-names(year_data)[ncol(year_data)] = 'Salary_to_predict' 
-
-
-# merge year and pre_year data
-data2014 <- merge(x = pre_year_data, y = year_data, by = c('Player_Full_Name', 'Team')) %>%
-  arrange(desc(Salary_to_predict))
-
-year_record = rep(2014, nrow(data2014))
-data2014 = cbind(year_record, data2014)
-
-data2014 <- data2014 %>% 
-  mutate(Salary_rank = floor((rank(data2014$Salary, ties.method = 'last')/nrow(data2012))*rank_stage- 0.00001) + 1) %>% 
-  mutate(Salary_to_predict_rank = floor((rank(data2014$Salary_to_predict, ties.method = 'last')/nrow(data2014))*rank_stage- 0.00001) + 1)
-############################################################
-# load data
-pre_year = 2014
-year = 2015
-
-pre_year_path = paste0('./merge_data/merge_data_', pre_year, '.csv')
-year_path = paste0('./merge_data/merge_data_', year, '.csv')
-
-pre_year_data = read_csv(pre_year_path)
-year_data = read_csv(year_path)
-
-
-year_data <- year_data %>%
-  select(Player_Full_Name,Team,Salary)
-names(year_data)[ncol(year_data)] = 'Salary_to_predict' 
-
-
-# merge year and pre_year data
-data2015 <- merge(x = pre_year_data, y = year_data, by = c('Player_Full_Name', 'Team')) %>%
-  arrange(desc(Salary_to_predict))
-
-year_record = rep(2015, nrow(data2015))
-data2015 = cbind(year_record, data2015)
-
-data2015 <- data2015 %>% 
-  mutate(Salary_rank = floor((rank(data2015$Salary, ties.method = 'last')/nrow(data2015))*rank_stage- 0.00001) + 1) %>% 
-  mutate(Salary_to_predict_rank = floor((rank(data2015$Salary_to_predict, ties.method = 'last')/nrow(data2015))*rank_stage- 0.00001) + 1)
-############################################################
-# load data
-pre_year = 2015
-year = 2016
-
-pre_year_path = paste0('./merge_data/merge_data_', pre_year, '.csv')
-year_path = paste0('./merge_data/merge_data_', year, '.csv')
-
-pre_year_data = read_csv(pre_year_path)
-year_data = read_csv(year_path)
-
-
-year_data <- year_data %>%
-  select(Player_Full_Name,Team,Salary)
-names(year_data)[ncol(year_data)] = 'Salary_to_predict' 
-
-
-# merge year and pre_year data
-data2016 <- merge(x = pre_year_data, y = year_data, by = c('Player_Full_Name', 'Team')) %>%
-  arrange(desc(Salary_to_predict))
-
-
-year_record = rep(2016, nrow(data2016))
-data2016 = cbind(year_record, data2016)
-
-data2016 <- data2016 %>% 
-  mutate(Salary_rank = floor((rank(data2016$Salary, ties.method = 'last')/nrow(data2016))*rank_stage- 0.00001) + 1) %>% 
-  mutate(Salary_to_predict_rank = floor((rank(data2016$Salary_to_predict, ties.method = 'last')/nrow(data2016))*rank_stage- 0.00001) + 1)
-
-############################################################
-
-
-bdata = do.call("rbind", list(data2012, data2013, data2014, data2015, data2016))
+bdata = do.call("rbind", year_data_list)
 
 bdata <- bdata %>%
   mutate(Trend = Salary_to_predict_rank - Salary_rank)
 
 bdata$Trend[bdata$Trend > 0] = 'up'
-bdata$Trend[bdata$Trend == 0] = 'hold'
-bdata$Trend[bdata$Trend < 0] = 'down'
+bdata$Trend[bdata$Trend <= 0] = 'hold&down'
+
+# bdata$Trend[bdata$Trend == 0] = 'hold'
+# bdata$Trend[bdata$Trend < 0] = 'down'
 
 # replace NA by -1
 bdata[is.na(bdata)] <- -1
@@ -168,6 +86,28 @@ bdata = cbind(cbind(bdata[,c(1,2,3,4,5,6,7)], pos_dummy), bdata[,seq(from = 8, t
 bdata = subset(bdata, pos_P == 0)
 
 cat('Data Count', nrow(bdata), '\n')
+
+
+
+
+
+
+bdata_rank_group <- bdata %>%
+  group_by(Salary_to_predict_rank) %>%
+  summarise(mean_Salary_to_predict = mean(Salary_to_predict))
+
+bdata_rank_group$Salary_to_predict_rank <- as.factor(bdata_rank_group$Salary_to_predict_rank)
+
+
+View(bdata_rank_group)
+
+rank_bar <- ggplot(bdata_rank_group, aes(x=Salary_to_predict_rank, y=mean_Salary_to_predict)) +
+  geom_bar(stat="identity", fill = 'deepskyblue') +
+  # geom_text(aes(label=mean_Salary_to_predict), vjust=-0.2) +
+  ggtitle("Salary Rank") + 
+  theme(text = element_text(family= 'Arial Unicode MS'))
+
+print(rank_bar)
 
 # "year_record"            "Player_Full_Name"       "Team"                   "Player"                
 # "PosOne_Detailed"        "PosOne"                 "PosTwo"                 "pos_1B"                
@@ -187,6 +127,12 @@ data_x = bdata[,c("G", "AB", "R", "H" , "TwoB", "ThreeB", "HR",
 data_y = bdata[,c("Trend")]
 data_y = as.factor(data_y)
 
+
+
+
+
+
+
 smp_size <- floor(0.9 * nrow(data_x))
 
 set.seed(46)
@@ -199,13 +145,69 @@ valid_y = data_y[-train_ind]
 
 
 
-brf = randomForest(x = train_x, 
-                   y = train_y,
-                   xtest = valid_x,
-                   ytest = valid_y
-                   )
+tree_nums = c(200,400,500,600,800,1000,1200,1500)
+ms = seq(from = 2, to = ncol(data_x), by = 2)
 
+best_tree_num = 0
+best_m = 0
+f1_all = c()
+
+for (tree_num in tree_nums){
+  for (m in ms){
+    brf = randomForest(x = train_x,
+                       y = train_y,
+                       ntree = tree_num,
+                       mtry = m,
+                       xtest = valid_x,
+                       ytest = valid_y)
+    print(brf)
+
+    conf = brf$test$confusion
+
+    # View(conf)
+    tp = conf[2,2]
+    fp = conf[1,2]
+    tn = conf[1,1]
+    fn = conf[2,1]
+
+    precision = tp / ( tp + fp )
+    recall = tp / ( tp + fn )
+
+    f1score = 2*precision*recall/(precision+recall)
+    print(f1score)
+
+    f1_all = c(f1_all, f1score)
+
+    if(which.max(f1_all) == length(f1_all)){
+      best_tree_num = tree_num
+      best_m = m
+    }
+
+  }
+}
+
+
+brf = randomForest(x = train_x,
+                   y = train_y,
+                   ntree = best_tree_num,
+                   mtry = best_m,
+                   xtest = valid_x,
+                   ytest = valid_y)
 print(brf)
+
+conf = brf$test$confusion
+
+# View(conf)
+tp = conf[2,2]
+fp = conf[1,2]
+tn = conf[1,1]
+fn = conf[2,1]
+
+precision = tp / ( tp + fp )
+recall = tp / ( tp + fn )
+
+f1score = 2*precision*recall/(precision+recall)
+cat('f1_score: ', f1score, '\n')
 
 
 
